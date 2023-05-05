2c377bc699aa1eecf9ddd33837a168e5d9f8b233
Neill Hamandishe <nhamandishenh@gmail.com>
2023-05-05 11:36:26 +0200

    [PARTIAL-FIX] Fix binding bug

--- ldap-client.js
+++ ldap-client.js
@@ -1,25 +1,73 @@
 import  ldap from "ldapjs";
+
 const {createClient} = ldap;
 
-
-
 const client = createClient({
-	url: ['ldap://192.168.20.10:1389']
+	url: ['ldap://192.168.20.10:389'],
+});
+
+client.on("connect", ()=>{
+
+	console.info("[INFO] Connection established.");
+
 });
 
 client.on("connectError", (err)=>{
-	console.log("connection error");
+
+	console.log("[ERROR] connection error");
+
+});
+
+client.on("setupError", (err)=>{
+
+	console.error("[ERROR] Setup error");
+
+});
+
+client.on("error", (err)=>{
+
+	console.error("[ERROR] General Error");
+
+});
+
+client.on("connectRefused", (err)=>{
+
+	console.error("[ERROR] Connection refused by AD server.");
+
+});
+
+client.on("timeout", (err)=>{
+
+	console.error(`[ERROR] Timeout ${err}`);
+
 });
 
 const bindLDAP = (username, password)=>{
-	var res = client.bind(`uid=${username}`, password, err => false);
-	console.log(res);
-	return true;
+	const dn = `ad\\${username}`;
+	
+	console.info("[INFO] Attempting bind...");
+
+	client.bind(dn, password, (err) => {
+		console.info(`[INFO] ad\${username}`);
+		if(err){
+
+			console.error("[ERROR] Failed to bind");
+			console.error(`[ERROR] 	${err}`);
+			client.unbind();
+			return;
+		}
+
+		console.info("[INFO] Bind success");
+
+	// 	searchLDAP(client);
+
+	});
 }
 
-const res = bindLDAP("neill.hamandishe", "#pass123");
-console.log(res);
+bindLDAP("Neill.Hamandishe", "#pass123");
 
-export {
+/*export {
+
 	bindLDAP
-}
+
+}*/
